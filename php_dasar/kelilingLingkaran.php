faa01290cd1a318126ff299f382dd2d635446ae3
Fatah Rizqi <fatahrzqp@gmail.com>
2025-05-07 18:50:25 +0700

    hapus folder php_dasar

--- php_dasar/kelilingLingkaran.php
+++
@@ -1,7 +0,0 @@
-<?php
-$jari = 15;
-$phi = 3.14;
-$keliling = 2 * $phi * $jari;
-
-echo "Keliling lingkaran dengan jari-jari $jari cm adalah $keliling cm.";
-?>
--- php_dasar/latihanVariabel.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
-
-foreach ($hari as $h) {
-    echo $h . "<br>";
-}
-?>
